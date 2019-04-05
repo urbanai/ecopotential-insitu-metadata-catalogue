2485d6f179fc48045965f453d3ac21cfa8fb349b
joana.simoes <joana.simoes@starlab.es>
2019-04-05 10:10:04 +0200

    - added flag -T to docker-compose

--- scripts/harvest_source.sh
+++ scripts/harvest_source.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source=$1
 
-docker-compose exec pycsw python bin/pycsw-admin.py -c harvest_source -f default.cfg -l $source
+docker-compose exec -T pycsw python bin/pycsw-admin.py -c harvest_source -f default.cfg -l $source
